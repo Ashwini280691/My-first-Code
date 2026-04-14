07efa6ccc1aa3688ad961347965026ba74fa615a
Ashwini280691 <ashwini.ar28@gmail.com>
2026-04-14 13:14:23 +0530

    new py if_statement file added

---
+++ if_statement.py
@@ -0,0 +1,13 @@
+print("Hi, Enter your marks: ")
+marks = int(input())
+if marks >= 90:
+    print("You got an A grade.")
+    print("Congratulations!You are a rockstar!")
+elif marks >= 80:
+    print("You got a B grade.")
+elif marks >= 70:
+    print("You got a C grade.")
+elif marks >= 60:
+    print("You got a D grade.")
+else:
+    print("You got an F grade.")
